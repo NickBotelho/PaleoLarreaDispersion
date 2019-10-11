adcb605b552328ecf11a8c67207c39cfeacc5e6e
NickBotelho <nbotelho@students.stonehill.edu>
2019-10-11 13:38:46 -0400

    latest code
    code up to an including niche overlap

--- IndependentResearch.R
+++ IndependentResearch.R
@@ -48,29 +48,24 @@ occdat_filtered = filter(occdat, latitude < 40) %>% filter (latitude > 20) %>%
 
 #sort(creosote_df2$date, decreasing = FALSE)
 
-
 #clim = raster::stack(list.files('/usr/share/data/wc2.0/bio2.5/', pattern='.tif', full.names = TRUE))
 #crop climate data down to north america
-
-
 
 #plot data points on a climate map 
 
 wc = getData('worldclim', var='bio', res = 5)
 
-
-
-ext=extent(c(-125, -100, 20, 40))
+ext=extent(c(-125, -85, 10, 40))
 wc2 = wc[[c('bio1', 'bio12', 'bio2', 'bio3')]] #annual factors (best so far)
 wc3 = wc[[c('bio5', 'bio14', 'bio17', 'bio18')]] #Dryness and heat factors
 wc4 = wc[[c('bio14', 'bio17', 'bio18')]] # extreme Dryness factors
 wc5 = wc[[c('bio5', 'bio9', 'bio10')]] #extreme heat factors
 wc6 = wc[[c('bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19')]] #all rain factors
 wc7 = wc[[c('bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'bio10', 'bio11')]] #all temp factors
-predictors = crop(wc2, ext)
+predictors = crop(wc2, paleo_ext)
 
 
-loc=occdat[,c('longitude', 'latitude')]
+loc=occdat_filtered[,c('longitude', 'latitude')]
 extr = extract(predictors, loc)
 loc = loc[!is.na(extr[,1]),]
 
@@ -89,7 +84,10 @@ loc = loc[-occ2thin,]
 eval = ENMevaluate(occ=as.data.frame(loc), env = predictors, method='block', parallel=FALSE, fc=c("L", "LQ"), RMvalues=seq(0.5, 2, 0.5), rasterPreds=T)
 best=which(eval@results$AICc == min(eval@results$AICc))
 
-plot(eval@predictions[[best]])
+
+
+
+plot(eval@predictions[[best]]) #green is high model score
 points(as.data.frame(loc), pch=20, cex =0.1)
 
 est.loc = extract(eval@predictions[[best]], as.data.frame(loc))
@@ -97,7 +95,7 @@ est.bg = extract(eval@predictions[[best]], eval@bg.pts)
 ev = evaluate(est.loc, est.bg)
 thr = threshold(ev)
 plot(eval@predictions[[best]] > thr$equal_sens_spec, col = c('lightgrey', 'black'))
-points(loc, pch = 20, cex= 1, col ='blue')
+points(loc, pch = 20, cex= 0.1, col ='blue')
 points(ratloc, pch = 20, cex= 1, col ='red')
 points(kangloc, pch = 20, cex = 1, col = 'green')
 
@@ -106,9 +104,83 @@ points(kangloc, pch = 20, cex = 1, col = 'green')
 
 
 
-###################
-usmap = map_data("usa")
-ggplot() + geom_polygon(data = usmap, 
-                        aes(x=long, y = lat, group=group, color = "black"), fill= "white") + 
-  coord_quickmap()  + geom_point(data = occdat_filtered, aes(x =longitude, y = latitude))
+###################Plotting Into South America
+diva_df = occ(query = "Larrea divaricata", 
+                  from = "gbif",
+                  limit = 1377,
+                  has_coords = TRUE)
+
+divaoccdat = occ2df(diva_df)
+divaoccdat = filter(divaoccdat, latitude < 15) %>% filter (latitude > -60) %>%
+  filter(longitude > -90) %>% filter(longitude < -35 )
+divaloc=divaoccdat[,c('longitude', 'latitude')]
+divaloc = divaloc[-occ2thin,]
+
+transfer = extent(c(-90, -35, -60, 15))
+trans.wc2 = crop(wc2, paleo_ext)
+trans.predict = predict(trans.wc2, eval@models[[best]], type = "cloglog")
+plot(trans.predict)
+points(divaloc, pch = 20, cex= 0.1, col ='black')
+
+#model performance on L. divaricata
+est.divaloc = extract(eval@predictions[[best]], as.data.frame(divaloc))
+est.bg = extract(eval@predictions[[best]], eval@bg.pts)
+ev.diva = evaluate(est.divaloc, est.bg)
+thr = threshold(ev)
+
+
+### calculate niche overlap between L. tridentata model and L. divaricata
+
+
+#ENMeval >  best model for divaricata
+diva_eval = ENMevaluate(occ=as.data.frame(divaloc), env = predictors, method='block', parallel=FALSE, fc=c("L", "LQ"), RMvalues=seq(0.5, 2, 0.5), rasterPreds=T)
+diva_best=which(diva_eval@results$AICc == min(diva_eval@results$AICc))
+#ENMeval > best model for tridentata
+best=which(eval@results$AICc == min(eval@results$AICc))
+
+
+trid_predict = predict(wc2, eval@models[[best]], type = "cloglog")
+diva_predict = predict(wc2, diva_eval@models[[best]], type = "cloglog")
+#no = nicheOverlap(pred.div, pred.tri, stat='D', mask=FALSE, checkNegatives=FALSE)
+no = nicheOverlap(diva_predict, trid_predict, 
+                  stat='D', mask=FALSE, checkNegatives=FALSE) 
+head(no)
+#Pulling paleo raster from the last glacial maximum 
+paleo_plots = raster::stack(list.files( pattern='.tif', full.names = TRUE)) #cclgmbi
+paleo_plots2 = paleo_plots[[c('cclgmbi1','cclgmbi2','cclgmbi3','cclgmbi12')]] #raster using same variables
+#as wc2 but with paleo data
+paleo_plots3 = paleo_plots[[c('cclgmbi5','cclgmbi14','cclgmbi17','cclgmbi18')]] #raster using same variables
+paleo_plots4 = paleo_plots[[c('cclgmbi14','cclgmbi17','cclgmbi18')]] #extreme dryness BEST
+paleo_plots5 = paleo_plots[[c('cclgmbi5','cclgmbi9','cclgmbi10')]] #extreme heat
+paleo_plots6 = paleo_plots[[c('cclgmbi13','cclgmbi14','cclgmbi15','cclgmbi17','cclgmbi16','cclgmbi18','cclgmbi19')]] #extreme heat
+paleo_plots7 = paleo_plots[[c('cclgmbi5','cclgmbi6','cclgmbi7','cclgmbi8','cclgmbi9','cclgmbi10','cclgmbi11')]] #extreme heat
+##The other paleo maps:
+second_paleo_plots = raster::stack(list.files( pattern='.tif', full.names = TRUE)) #melgmbi
+second_paleo_plots2 = second_paleo_plots[[c('melgmbi1','melgmbi2','melgmbi3','melgmbi12')]]
+second_paleo_plots4 = second_paleo_plots[[c('melgmbi14','melgmbi17','melgmbi18')]]
+
+third_paleo_plots = raster::stack(list.files( pattern='.tif', full.names = TRUE))  #mrlgmbi
+third_paleo_plots2 = third_paleo_plots[[c('mrlgmbi1','mrlgmbi2','mrlgmbi3','mrlgmbi12')]]
+third_paleo_plots4 = third_paleo_plots[[c('mrlgmbi14','mrlgmbi17','mrlgmbi18')]]
+
+
+
+paleo_ext = extent(c(-125, -20, -60, 40)) #Bounds to see from southern US -- South SA
+paleo_central_america_ext = extent(c(-125, -25, -40, 30)) #Bounds to see from southern US -- South SA
+
+names(paleo_plots2) = c('bio1', 'bio2', 'bio3', 'bio12')
+paleo.paleoplots2 = crop(paleo_plots2, paleo_ext)
+paleo.predict = predict(paleo.paleoplots2, eval@models[[best]], type = "cloglog")
+plot(paleo.predict)
+###using the two new paleo maps
+names(second_paleo_plots2) = c('bio1', 'bio2', 'bio3', 'bio12')
+paleo.second_paleoplots2 = crop(second_paleo_plots2, paleo_ext)
+paleo.predict2 = predict(paleo.second_paleoplots2, eval@models[[best]], type = "cloglog")
+plot(paleo.predict2)
+
+names(third_paleo_plots2) = c('bio1', 'bio2', 'bio3', 'bio12')
+paleo.third_paleoplots2 = crop(third_paleo_plots2, paleo_ext)
+paleo.predict3 = predict(paleo.third_paleoplots2, eval@models[[best]], type = "cloglog")
+plot(paleo.predict3)
+
 
